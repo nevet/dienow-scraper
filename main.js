2df0575db5f9377688f0d17127ae8746684390c6
Huang Da <nevets.st@gmail.com>
2015-08-06 01:34:00 +0800

    add dependencies

---
+++ main.js
@@ -0,0 +1,12 @@
+// dependencies
+
+// file utils
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+// dynamic scraper
+var zombie = require('zombie');
+
+// static html process
+var request = require("request");
+var cheerio = require("cheerio");
